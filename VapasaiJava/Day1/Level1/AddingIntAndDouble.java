b2133af263d165f872c2957a4c89036c69da5be3
kavithmk84 <79688563+kavithmk84@users.noreply.github.com>
2021-03-03 16:41:08 +0530

    Add files via upload

---
+++ VapasaiJava/Day1/Level1/AddingIntAndDouble.java
@@ -0,0 +1,14 @@
+package mar1.examples;
+
+public class AddingIntAndDouble {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5;
+		double d = 6.5;
+		double add = a + d;
+		System.out.println("Added value is:"+add);
+
+	}
+
+}
