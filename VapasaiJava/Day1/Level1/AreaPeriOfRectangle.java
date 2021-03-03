f4c8078d61b133dd09cb7f911ac01b843bd037b9
kavithmk84 <79688563+kavithmk84@users.noreply.github.com>
2021-03-03 16:46:24 +0530

    Add files via upload

---
+++ VapasaiJava/Day1/Level1/AreaPeriOfRectangle.java
@@ -0,0 +1,16 @@
+package mar1.examples.Operators;
+
+public class AreaPeriOfRectangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double length = 10.2;
+		double breadth = 5.3;
+		double area = length * breadth;
+		System.out.println("Area of rectangle:"+area);
+		double peri = 2 * (length + breadth);
+		System.out.println("Perimeter of the rectangle:"+peri);
+
+	}
+
+}
