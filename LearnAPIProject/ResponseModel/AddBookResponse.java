a5f739cfd7398fbe732cf5f40444989809a73ec5
kavithmk84 <79688563+kavithmk84@users.noreply.github.com>
2021-03-15 14:40:31 +0530

    Create AddBookResponse.java

---
+++ LearnAPIProject/ResponseModel/AddBookResponse.java
@@ -0,0 +1,21 @@
+package ResponseModel;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+public class AddBookResponse {
+    @JsonProperty("Msg")
+    private String msg;
+    @JsonProperty("ID")
+    private String id;
+    public String getMsg() {
+        return msg;
+    }
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+}
